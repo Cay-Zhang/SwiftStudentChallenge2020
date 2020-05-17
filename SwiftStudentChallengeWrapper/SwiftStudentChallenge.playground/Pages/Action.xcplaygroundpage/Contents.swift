cf80f59277b0779cfcaf4b1ca608b59d56025a41
Cay Zhang <zcy20020811@gmail.com>
2020-05-17 02:19:17 -0700

    Docs: Action & Map

--- SwiftStudentChallengeWrapper/SwiftStudentChallenge.playground/Pages/Action.xcplaygroundpage/Contents.swift
+++ SwiftStudentChallengeWrapper/SwiftStudentChallenge.playground/Pages/Action.xcplaygroundpage/Contents.swift
@@ -1,12 +1,72 @@
 //: [Previous](@previous)
+//: # The Action System
+/*:
+ Run all code on the page by clicking *Execute Playground* button on the debug area tool bar and see what happens. Describe what the bird is doing.
+ 
+ Oh no! Invisible bird!
+ 
+ Do we need to write a bunch of imperative code to hide/show the bird and create some timers to make it repeat forever?
+ 
+ Well, the `Action` turns out to be just like a text description.
+ */
 
-import SpriteKit
-import SwiftUI
+let birdAction: Action = Actions(running: .sequentially) {
+    Fade(.out, duration: 0.5)
+    Wait(forRandomDurationIn: 0.5 ... 1.5)
+    Fade(.in, duration: 0.5)
+    Wait(forRandomDurationIn: 0.5 ... 1.5)
+}.repeatForever()
+
+/*:
+ An `Action` can apply effects to its target. Here is a list of most of the actions supported:
+ 
+ ### Grouping
+ `Actions` is a special action that groups several actions together. You can specify the way its sub-actions are executed (sequentially or parallelly).
+ 
+     Actions(running: .sequentially/.parallelly) {
+        action1
+        action2
+     }
+ 
+ ### Movement & Transform
+ - `MoveBy(x:y:duration:)`
+ - `Wiggle(amplitude:duration:)`
+ - `Rotate(by:duration:)` (counterclockwise)
+ - `Scale(to:duration:)`
+ 
+ ### Opacity & Color
+ - `Fade(.in/.out, duration:)`
+ - `Colorize(_:duration:colorBlendFactor:)` (`colorBlendFactor` is a number from 0 to 1 indicating how much the color is blended in the texture of the target)
+ - `Decolorize()`
+ 
+ ### Time
+ - `Wait(forDuration:)`
+ - `Wait(forRandomDurationIn:)` (takes a closed range of `TimeInterval` as its parameter)
+ 
+ ### Repetition
+ If you want an action to repeat, attach the following to the end of the action you want to repeat:
+ 
+ - `.repeat(_:)`
+ - `.repeatForever()`
+ 
+ */
 
 
+let game = Game {
+    Level(name: "Bird Action") {
+        Pipes(30, constantInterval: 1, constantPipeGap: 150)
+    }.birdAction(birdAction)
+    // ↑ The defined bird action is attached to the level
+}
 
+game.runLevels()
 
-
-
+/*:
+ `birdAction` is then attached to the level using its `birdAction` property. The target for this action is the bird, meaning that only the bird will be affected by the instructions defined in your action.
+ 
+ Now, you can try to implement some bird actions for yourself!
+ 
+ For example, try moving the bird horizontally and observe the effect. You'll find a implementation of this on the last page.
+ */
 
 //: [Next](@next)
--- SwiftStudentChallengeWrapper/SwiftStudentChallenge.playground/Pages/Basics.xcplaygroundpage/Contents.swift
+++ SwiftStudentChallengeWrapper/SwiftStudentChallenge.playground/Pages/Basics.xcplaygroundpage/Contents.swift
@@ -5,11 +5,20 @@
  
  In fact, the game itself doesn't require much explanation. All you do is tapping/clicking the screen to make the bird flap.
  
- Sounds easy, right? Run the code to show a plain level and try it yourself.
+ Sounds easy, right? Run the code to show a plain game and try it yourself.
+ 
+ If you see a black screen, just run it again.
  */
 let sampleGame = Game {
+    // ↓ Levels
     Level(name: "Plain and Boring") {
-        Pipes(30, constantInterval: 1, constantPipeGap: 150)
+        // ↓ Map Components
+        Pipes(10, constantInterval: 1, constantPipeGap: 150)
+    }
+    Level(name: "Plain and Boring, but Harder") {
+        // ↓ Map Components
+        Pipes(5, constantInterval: 1, constantPipeGap: 150)
+        Pipes(10, constantInterval: 0.7, constantPipeGap: 125)
     }
 }
 
@@ -29,6 +38,14 @@ sampleGame.runLevels()
  The moving content on your screen tricks you to believe that the bird is moving horizontally through the immobile map, but the fact is the other way around.
  The pipes are spawned initially to the right of the scene and are moved to the left of it while the bird doesn't move horizontally at all!
  That's why it says "interval". You are directly controlling the **spawn interval** of the pipes instead of the **distance** among them.
+ 
+ As you can see, you can arrange more than one `MapComponent` in a level. Where are they placed? Or "time-basedly", when are they spawned?
+ 
+ A **MapComponent** can define its own "time span" (duration) and its time span starts immediately after the previous one.
+ 
+ `Pipes` is the main map component in this game. It defines its duration as the sum of all the intervals for the pipes (for a constant interval, its duration would be `count` * `constantInterval`).
+ 
+ Before learning about more map components, in the next page, you will familiarize yourself with the **Action system** which is used throughout the customization of this game.
  
  ## More about Difficulty
  There isn't a built-in difficulty scale available for you to tweak since this playground aims to offer more freedom in level design and encourage creativity and craziness. However, you can always design a difficulty adjustment system for yourself.
